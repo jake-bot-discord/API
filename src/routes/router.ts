969e79c331b2a34f7e23e2502f2d3662bba27d6c
Bianca de Castro <biancadcastro2501@gmail.com>
2023-05-12 23:56:41 -0300

    corrigindo um erro

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,5 @@
 import { app } from "../index";
 import { formater } from "../util/formater";
-import { itemdata } from "./itemdata";
 import { HostTurnOff, HostTurnOn } from "./host";
 import authRouter from "./auth/index"
 import guildsRouter from "./guilds/index"
@@ -9,14 +8,6 @@ app.get('/', (req, res) => {
     res.status(200)
     res.send("Hello world!")
 });
-
-app.route('/itemdata')
-    .get((req, res) => {
-        if (req.headers.authorization !== process.env.ITEMS_PASSWORD)
-            return res.status(401).send('Acesso negado!')
-
-        return itemdata(req, res)
-    });
 
 app.post('/sharderror', (req, res) => {
     if (req.headers.authorization !== process.env.WEBHOOKS_PASSWORD)
