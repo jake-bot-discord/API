09f148cb688ce7b00e934556de04cb4896cc4878
Bianca de Castro <biancadcastro2501@gmail.com>
2023-07-22 00:38:50 -0300

    corrigindo codigos http

--- src/util/middleware.ts
+++ src/util/middleware.ts
@@ -5,11 +5,11 @@ export const isAuthenticated = (req: Request, res: Response, next: NextFunction)
 export const globalMiddleWare = async (req: Request, res: Response, next: NextFunction) => {
     await console.log(req.headers.token);
 
+    if ( !req.headers.token ) return res.status(401).send("Token not found")
+
     if (req.headers.token == process.env.GLOBAL_TOKEN) {
-        console.log("Autorizado")
         return next()
     } else {
-        console.log("Não autorizado")
         return res.status(401).send("Access denied")
     }
 }
