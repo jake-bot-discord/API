92ac9d36bcd5e8f916e8d0fe90c77259f6643fbb
Bianca de Castro Mendonça <biancadcastro2501@gmail.com>
2024-01-18 19:15:55 -0300

    removendo os arquivos antigos

--- src/functions/discordClient.ts
+++
@@ -1,7 +0,0 @@
-import {Client, GatewayIntentBits, Events} from "discord.js"
-
-export const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-
-client.once(Events.ClientReady, client => {})
-
-client.login(process.env.DISCORD_BOT_TOKEN)
--- src/util/types.ts
+++
@@ -1,8 +0,0 @@
-export type PartialGuild = {
-    id: string,
-    name: string,
-    icon: string,
-    owner: string,
-    permissions: string,
-    features: string[],
-}
