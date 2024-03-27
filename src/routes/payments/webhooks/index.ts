7485f62c4ca8b2ac6dfe82a8bf9e152f23b509da
Bianca de Castro <biancadcastro2501@gmail.com>
2024-03-27 17:44:26 -0300

    criando a handler das webhooks

--- src/routes/payments/webhooks/index.ts
+++ src/routes/payments/webhooks/index.ts
@@ -1,31 +1,27 @@
 import { FastifyInstance, FastifyReply } from "fastify";
-import { succeededPayment } from "../../../controllers/stripe/customer";
 import stripe from "stripe"
 import { z } from "zod"
-import { create } from "./invoices/create";
+import { SubscriptionCreate } from "./subscription/create";
+import { SubscriptionUpdate } from "./subscription/update";
+import { SubscriptionDelete } from "./subscription/delete";
+import { CustomerCreate } from "./customer/create";
+import { CustomerDelete } from "./customer/delete";
+import { CustomerUpdate } from "./customer/update";
 const stringPath = z.string()
 
 export const handler = async (app: FastifyInstance, req: any, rep: FastifyReply) => {
-    const Stripe = new stripe(stringPath.parse(process.env.STRIPE_SECRET_TOKEN), {
-        apiVersion: "2023-10-16",
-    })
     const event = req.body
 
-    console.log(event.type)
-
-    if (event.type) {
-        if(event.type == "invoice.created") return create(app, req, rep, event)
+    const eventCase: any = {
+        "customer.created": CustomerCreate, 
+        "customer.deleted": CustomerDelete, 
+        "customer.updated": CustomerUpdate, 
+        "customer.subscription.created": SubscriptionCreate,
+        "customer.subscription.updated": SubscriptionUpdate,
+        "customer.subscription.deleted": SubscriptionDelete,
     }
 
-    // switch (event.type) {
-    //     case 'payment_intent.succeeded': 
-    //         const paymentIntent = event.data
-
-    //         const stripeCustomer: any = await Stripe.customers.retrieve(paymentIntent.customer)
-
-    //         console.log(stripeCustomer)
-    //     break;
-    // }
+    if ( event.type ) return eventCase[event.type](app, req, rep, event)
 
     return rep.status(200).send("ok")
 }
