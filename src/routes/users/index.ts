d48187e07508ef4ebf99282b2b5968a9cbfab5d2
Bianca de Castro <biancadcastro2501@gmail.com>
2024-03-27 17:42:24 -0300

    removendo rotas inutilizadas

--- src/routes/users/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
-import { developers } from "./developers";
-
-export const users = (app: FastifyInstance, opts: FastifyPluginOptions, done: () => void) => {
-
-    app.get("/developers", (req: FastifyRequest, rep: FastifyReply) => developers(app, req, rep))
-    
-    done()
-}   
