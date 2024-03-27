53a0bd948e084752a7c8da0e62eccf7067ba18d5
Bianca de Castro <biancadcastro2501@gmail.com>
2024-03-27 18:44:35 -0300

    criando os tipos para o objeto de fatura

---
+++ src/utils/types/invoices.ts
@@ -0,0 +1,10 @@
+export type InvoiceType = {
+    id: String,
+    created: Number,
+    hosted_invoice_url: any,
+    invoice_pdf: any,
+    lines: String,
+    paid: Boolean,
+    status: String,
+    total: Number
+}
